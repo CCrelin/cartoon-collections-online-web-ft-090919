948b9a036a7cba4c18976d1b0fbeec47a7be725e
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-16 23:58:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,15 @@
 def roll_call_dwarves# code an argument here
- array.each.with_index(1) do |dwarfs, index|
+  array.each.with_index(1) do |dwarfs, index|
     puts "#{index} . #{dwarfs}"
   end
 end
 
 def summon_captain_planet# code an argument here
-array.map do |caps|
+  array.map do |caps|
     caps.capitalize + "!"
+  end
+caps.upcase + "!"
 end
-
 def long_planeteer_calls# code an argument here
   # Your code here
 end
