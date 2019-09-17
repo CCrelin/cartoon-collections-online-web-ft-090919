4711e77a9f728b6d677d3046b5aca4b75643d790
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-17 00:11:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,4 +21,9 @@ def long_planeteer_calls(array)
     end 
   end
 
-
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+ array.find do |queijo|
+  cheese_types.include? queijo
+   end
+end
